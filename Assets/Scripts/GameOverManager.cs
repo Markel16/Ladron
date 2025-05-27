2500a50994531a0843f07a288d487bbef5cd560d
Markel16 <markel_nunez@euneiz.com>
2025-05-27 20:36:34 +0200

    menu de GameOver finalizado al 100%

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,34 +1,46 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameOverManager : MonoBehaviour
 {
     public static GameOverManager instance;
     public GameObject gameOverPanel;
 
-    [SerializeField] private string menuSceneName = "Scene1Menu"; // depende de la escena en la que este el jugador
+    [SerializeField] private string menuSceneName = ""; // Asignable desde el inspector (opcional)
 
     private void Awake()
     {
         instance = this;
+
+        if (string.IsNullOrEmpty(menuSceneName))
+        {
+            menuSceneName = SceneManager.GetActiveScene().name;
+        }
     }
 
     public void ShowGameOverPanel()
     {
-        Time.timeScale = 0f;
+        Debug.Log("se ha llamado a ShowGameOverPanel");
         gameOverPanel.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        // Espera 2 segundos y luego va al menú del nivel
-        Invoke(nameof(ReturnToLevelMenu), 2f);
+        StartCoroutine(WaitAndLoadMenu());
     }
 
-    void ReturnToLevelMenu()
+    private IEnumerator WaitAndLoadMenu()
+    {
+        yield return new WaitForSecondsRealtime(2f); // espera real incluso si Time.timeScale = 0
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    
+    public void ReturnToLevelMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(menuSceneName);
     }
 }
-
 
